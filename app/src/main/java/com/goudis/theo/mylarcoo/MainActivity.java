cd7e3c9fe975292022152487d28b6e12763947e0
Theo Goudis <theogoudis@gmail.com>
2015-11-04 18:08:33 +0200

    small changes, removed some dependencies

--- app/src/main/java/com/goudis/theo/mylarcoo/MainActivity.java
+++ app/src/main/java/com/goudis/theo/mylarcoo/MainActivity.java
@@ -8,18 +8,16 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.AppBarLayout;
 
 public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener{
 
-	private static final int PERCENTAGE_TO_ANIMATE_AVATAR = 20;
-	private boolean mIsAvatarShown = true;
-
-	private ImageView mProfileImage;
+	private static final int PERCENTAGE_TO_ANIMATE_LOGO = 50;
+	private boolean mIsLogoShown = true;
+	
+	private ImageView mLogoView;
 	private int mMaxScrollSize;
 
 	@Override
@@ -30,14 +28,7 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
 		TabLayout tabLayout = (TabLayout) findViewById(R.id.materialup_tabs);
 		ViewPager viewPager  = (ViewPager) findViewById(R.id.materialup_viewpager);
 		AppBarLayout appbarLayout = (AppBarLayout) findViewById(R.id.materialup_appbar);
-		mProfileImage = (ImageView) findViewById(R.id.materialup_profile_image);
-
-		Toolbar toolbar = (Toolbar) findViewById(R.id.materialup_toolbar);
-		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-			@Override public void onClick(View v) {
-				onBackPressed();
-			}
-		});
+		mLogoView = (ImageView) findViewById(R.id.materialup_profile_image);
 
 		appbarLayout.addOnOffsetChangedListener(this);
 		mMaxScrollSize = appbarLayout.getTotalScrollRange();
@@ -57,15 +48,14 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
 
 		int percentage = (Math.abs(i)) * 100 / mMaxScrollSize;
 
-		if (percentage >= PERCENTAGE_TO_ANIMATE_AVATAR && mIsAvatarShown) {
-			mIsAvatarShown = false;
-			mProfileImage.animate().scaleY(0).scaleX(0).setDuration(200).start();
+		if (percentage >= PERCENTAGE_TO_ANIMATE_LOGO && mIsLogoShown) {
+			mIsLogoShown = false;
+			mLogoView.animate().scaleY(0).scaleX(0).setDuration(200).start();
 		}
 
-		if (percentage <= PERCENTAGE_TO_ANIMATE_AVATAR && !mIsAvatarShown) {
-			mIsAvatarShown = true;
-
-			mProfileImage.animate()
+		if (percentage <= PERCENTAGE_TO_ANIMATE_LOGO && !mIsLogoShown) {
+			mIsLogoShown = true;
+			mLogoView.animate()
 					.scaleY(1).scaleX(1)
 					.start();
 		}
@@ -78,7 +68,7 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
 
 		@Override
 		public int getCount() {
-			return 2;
+			return 3;
 		}
 
 		@Override
@@ -86,6 +76,7 @@ public class MainActivity extends AppCompatActivity implements AppBarLayout.OnOf
 			switch(i) {
 				case 0: return CardsFragment.newInstance();
 				case 1: return CardsFragment.newInstance();
+				case 2: return CardsFragment.newInstance();
 			}
 			return null;
 		}
